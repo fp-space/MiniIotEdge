8b119aa8001aeb0e2f9c64303e23c07fd0ff3d64
TheFoxFairy <1479852727@qq.com>
2024-11-26 19:03:44 +0800

    fix(connector): 修复mqtt发送错误

--- iot-edge-connector/message-broker-module/src/main/java/com/iothub/message/broker/module/config/MqttAdapterConfig.java
+++ iot-edge-connector/message-broker-module/src/main/java/com/iothub/message/broker/module/config/MqttAdapterConfig.java
@@ -3,6 +3,7 @@ package com.iothub.message.broker.module.config;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.mqttv5.client.MqttClient;
 import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
+import org.eclipse.paho.mqttv5.common.MqttException;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +16,10 @@ import org.springframework.integration.mqtt.inbound.Mqttv5PahoMessageDrivenChann
 import org.springframework.integration.mqtt.outbound.Mqttv5PahoMessageHandler;
 import org.springframework.integration.mqtt.support.MqttHeaderMapper;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.converter.SmartMessageConverter;
 import org.springframework.messaging.converter.StringMessageConverter;
+
+import java.util.Objects;
 
 @Configuration
 @Slf4j
@@ -29,12 +31,14 @@ public class MqttAdapterConfig {
         this.mqttConfigProperties = mqttConfigProperties;
     }
     
+    
     /**
      * 配置 MQTT 连接选项
      */
-    private MqttConnectionOptions createMqttConnectionOptions() {
+    @Bean
+    public MqttConnectionOptions mqttConnectionOptions() {
         MqttConnectionOptions options = new MqttConnectionOptions();
-        options.setServerURIs(new String[]{mqttConfigProperties.brokerUrl()});
+        options.setServerURIs(new String[]{Objects.requireNonNull(mqttConfigProperties).brokerUrl()});
         options.setCleanStart(true);
         options.setConnectionTimeout(30); // 默认连接超时30秒
         options.setKeepAliveInterval(60); // 默认保持活动时间60秒
@@ -44,14 +48,28 @@ public class MqttAdapterConfig {
         return options;
     }
     
-    /**
-     * 提取 Mqttv5ClientManager 为单独的 @Bean
-     */
+    // 配置 MQTT 客户端连接信息
     @Bean
-    public Mqttv5ClientManager mqttv5ClientManager() {
-        MqttConnectionOptions options = createMqttConnectionOptions();
-        return new Mqttv5ClientManager(options, mqttConfigProperties.clientId());
+    public MqttClient mqttClient(MqttConnectionOptions mqttConnectionOptions) throws MqttException {
+        String clientId = mqttConfigProperties.clientId();
+        String brokerUrl = mqttConfigProperties.brokerUrl();
+        
+        // 创建 MQTT 客户端实例
+        MqttClient client = new MqttClient(brokerUrl, clientId);
+        
+        client.connect(mqttConnectionOptions);
+        return client;
     }
+    
+    // 配置 Mqttv5ClientManager
+    @Bean
+    public Mqttv5ClientManager mqttv5ClientManager(MqttClient mqttClient,MqttConnectionOptions mqttConnectionOptions) {
+        Mqttv5ClientManager mqttv5ClientManager = new Mqttv5ClientManager(mqttConnectionOptions, mqttClient.getClientId());
+        mqttv5ClientManager.start();
+        return mqttv5ClientManager;
+    }
+    
+    
     
     /**
      * 接收消息的通道
@@ -65,12 +83,12 @@ public class MqttAdapterConfig {
      * 配置 MQTT 5 客户端管理器及消息适配器
      */
     @Bean
-    public MessageProducer inbound() {
+    public MessageProducer inbound(MqttConnectionOptions mqttConnectionOptions) {
         String[] subTopicArr = mqttConfigProperties.subscriptions().toArray(new String[0]);
         
         // 使用 clientManager 获取客户端并创建消息适配器
         Mqttv5PahoMessageDrivenChannelAdapter adapter = new Mqttv5PahoMessageDrivenChannelAdapter(
-                createMqttConnectionOptions(),
+                mqttConnectionOptions,
                 mqttConfigProperties.clientId(),
                 subTopicArr
         );
@@ -89,27 +107,7 @@ public class MqttAdapterConfig {
     }
     
     
-    /**
-     * 配置发送消息的处理器
-     */
-    @Bean(name = "mqttOutbound")
-    @ServiceActivator(inputChannel = "mqttOutputChannel")
-    public MessageHandler mqttOutbound(Mqttv5ClientManager mqttv5ClientManager) {
-        String clientId = "mqttOutbound-" + mqttConfigProperties.clientId();  // 生成唯一的客户端ID
-        log.info("MQTT消息处理器（生产者）clientId:{}", clientId);
-        
-        // 创建消息发送处理器
-        Mqttv5PahoMessageHandler messageHandler = new Mqttv5PahoMessageHandler(mqttv5ClientManager);
-        messageHandler.setHeaderMapper(new MqttHeaderMapper());
-        messageHandler.setAsync(true);  // 设置异步发送
-        messageHandler.setDefaultQos(mqttConfigProperties.qos());  // 设置默认QoS
-        
-        // 使用 SmartMessageConverter 替换默认的消息转换器
-        SmartMessageConverter smartMessageConverter = new StringMessageConverter();
-        messageHandler.setConverter(smartMessageConverter);   // 设置消息转换器
-        
-        return messageHandler;
-    }
+    
     
     /**
      * 配置发送消息的通道（生产者）
@@ -119,19 +117,24 @@ public class MqttAdapterConfig {
         return new DirectChannel();  // 直接通道
     }
     
-    
-    // 配置 MQTT 客户端连接信息
-    @Bean
-    public MqttClient mqttClient() throws Exception {
-        // 生成客户端ID，客户端ID应该唯一
-        String clientId = "mqtt-client-" + System.currentTimeMillis();
-        
-        // 创建 MqttClient 实例
-        MqttClient mqttClient = new MqttClient(mqttConfigProperties.brokerUrl(), clientId);
-        
-        // 连接到 MQTT Broker
-        mqttClient.connect(createMqttConnectionOptions());
-        
-        return mqttClient;
+    /**
+     * 配置发送消息的处理器
+     */
+    @Bean(name = "mqttOutbound")
+    public Mqttv5PahoMessageHandler mqttOutbound(MqttConnectionOptions  mqttConnectionOptions) {
+        log.info("MQTT消息处理器（生产者）clientId:{}", mqttConfigProperties.clientId());
+
+        // 创建消息发送处理器
+        Mqttv5PahoMessageHandler messageHandler = new Mqttv5PahoMessageHandler(mqttConnectionOptions,"Out-" +mqttConfigProperties.clientId());
+        messageHandler.setHeaderMapper(new MqttHeaderMapper());
+        messageHandler.setAsync(true);  // 设置异步发送
+        messageHandler.setDefaultQos(mqttConfigProperties.qos());  // 设置默认QoS
+
+        // 使用 SmartMessageConverter 替换默认的消息转换器
+        SmartMessageConverter smartMessageConverter = new StringMessageConverter();
+        messageHandler.setConverter(smartMessageConverter);   // 设置消息转换器
+
+        return messageHandler;
     }
+
 }
