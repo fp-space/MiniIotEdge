906823220069766e06db89528bf2f81a7d4fead6
TheFoxFairy <1479852727@qq.com>
2024-11-27 10:17:33 +0800

    fix(mqtt): 修复mqtt连接丢失问题

--- iot-edge-connector/message-broker-module/src/main/java/com/iothub/message/broker/module/manager/DeviceManager.java
+++ iot-edge-connector/message-broker-module/src/main/java/com/iothub/message/broker/module/manager/DeviceManager.java
@@ -3,9 +3,8 @@ package com.iothub.message.broker.module.manager;
 import com.iothub.message.broker.module.entity.Device;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
